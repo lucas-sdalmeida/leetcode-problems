19f15c33df5a9a26ada78bc43b06795df4b1ba13
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-09-03 11:12:50 -0300

    feat: implemented solutions to fit the test cases

--- python_solutions/python_solutions/leetcodedaily/uniquepaths/solution.py
+++ python_solutions/python_solutions/leetcodedaily/uniquepaths/solution.py
@@ -1,3 +1,12 @@
 class Solution:
+    def __init__(self):
+        self._memo_ = {}
+
     def unique_paths(self, m, n):
-        pass
+        if m == n == 1:
+            return 0
+        if m == 1 or n == 1:
+            return 1
+        if (m, n) not in self._memo_.keys():
+            self._memo_[m, n] = self.unique_paths(m - 1, n) + self.unique_paths(m, n - 1)
+        return self._memo_[m, n]
